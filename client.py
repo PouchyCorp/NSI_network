49e22b7647012e31a68eabd8baedff059d7cd23a
PouchyCorp <pouchy.l.acarien@gmail.com>
2024-03-06 17:03:42 +0100

    functional real-time 60fps comm

--- client.py
+++ client.py
@@ -1,20 +1,38 @@
 from socket import socket
+from time import sleep
+import pygame as pg
+from random import randint
 
 print('starting')
 serveur = socket()
 serveur.connect( ('192.168.56.1', 16384) )
 ############
 
-while True:
-    reponse = input("> ")
-    serveur.send(reponse.encode())
+WIN = pg.display.set_mode((1000,1000))
 
+run = True
+while run:
     message = serveur.recv(1000)
-    texte = message.decode()
-    print(texte)
-    if texte == 'end':
-        break
-    #print(adclient," : \n",donnees.decode(), end='')
+
+    keys = pg.key.get_pressed()
+
+    reponse = "0"
+    rand = 0
+    if keys[pg.K_LEFT] or rand == 1:
+        reponse = "LEFT"
+    if keys[pg.K_RIGHT] or rand == 2:
+        reponse = "RIGHT"
+    if keys[pg.K_DOWN] or rand == 3:
+        reponse = "DOWN"
+    if keys[pg.K_UP] or rand == 4:
+        reponse = "UP"
+
+    serveur.send(reponse.encode())
     
+    for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+                break
 
 serveur.close()
+pg.quit()
--- serveur.py
+++ serveur.py
@@ -1,4 +1,6 @@
 from socket import socket
+from time import sleep
+import pygame as pg
 
 print('starting...')
 serveur = socket()
@@ -8,19 +10,46 @@ serveur.listen()
 if sclient:
     print('connected')
 
+WIN = pg.display.set_mode((1000,1000))
+clock = pg.time.Clock()
+
+rectX = 100
+rectY = 100
+player = pg.Rect(50,50,rectX,rectY)
+
+def draw(player):
+    WIN.fill('black')
+    pg.draw.rect(WIN, "red", player)
+    pg.display.update()
+
 ##########
-while True:
-    message = sclient.recv(1000)
-    texte = message.decode()
-    print(texte)
-    #print(adclient," : \n",donnees.decode(), end='')
-    if texte == 'end':
-        break
+run = True
+while run:
+    clock.tick(60)
 
-    reponse = input("> ")
+    reponse = '0'
     sclient.send(reponse.encode())
-    if reponse == 'end':
-        break
-    
-sclient.close()
 
+    message = sclient.recv(1000)
+
+    reponse = message.decode()
+    print(reponse)
+
+    for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+                break
+    
+    if reponse == "LEFT":
+        player.x -= 1
+    if reponse == "RIGHT":
+        player.x += 1
+    if reponse == "UP":
+        player.y -= 1
+    if reponse == "DOWN":
+        player.y += 1
+
+    draw(player)
+
+sclient.close()
+pg.quit()
