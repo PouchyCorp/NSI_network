6cb424d55a1af9225db20ad366b1ae7ac164729a
PouchyCorp <pouchy.l.acarien@gmail.com>
2024-04-25 00:01:49 +0200

    level editor better grid system

--- level_editor.py
+++ level_editor.py
@@ -11,17 +11,16 @@ def save():
 
 def snap(coor,r):
     if coor % r != 0:
-        if coor % r < r/2:
-            coor -= coor % r
-        else:
-            coor += coor % r
-    return coor
+        coor -= coor % r
+    return int(coor)
 
 def main():
     p.init()
     WIN = p.display.set_mode((1000, 1000))
     p.display.set_caption("TAGLIATELE Level Editor")
     grid = p.image.load('assets/background.jpg')
+    WIDTH, HEIGHT = WIN.get_size()
+    gridXSize, gridYSize = HEIGHT/50, HEIGHT/50
     def wall(x1,y1,x2,y2):
         try:
             if x2 > x1 and y2 > y1:
@@ -49,6 +48,14 @@ def main():
         
     def draw():
         WIN.blit(grid,(0,0))
+        x = 0
+        for i in range(50):
+            p.draw.line(WIN,(10,10,10),(0+x,0),(0+x,WIDTH))
+            x+=gridXSize
+        x = 0
+        while x<=WIDTH:
+            p.draw.line(WIN,(10,10,10),(0,0+x),(WIDTH,0+x))
+            x+=gridXSize
         for wall in walls:
             p.draw.rect(WIN,wall["color"],wall["rect"])
         p.display.update()
@@ -59,7 +66,7 @@ def main():
     clicked = False
     color = ""
     color_fill = ""
-    r = 6
+    r = gridXSize
     while running :
         
         for event in p.event.get() :
@@ -86,8 +93,10 @@ def main():
                     color, color_fill = "blue", "blue"
 
             if clicked == True:
-                    ghostSurfacePos : tuple = surfacePos(x1,y1,p.mouse.get_pos()[0],p.mouse.get_pos()[1])
-                    ghostSurface = p.Surface((abs(p.mouse.get_pos()[0]-x1),abs(p.mouse.get_pos()[1]-y1)))
+                    snappedMouseX, snappedMouseY = snap(p.mouse.get_pos()[0],r) , snap(p.mouse.get_pos()[1],r)
+                    ghostSurfacePos : tuple = surfacePos(x1,y1,snappedMouseX,snappedMouseY)
+                    
+                    ghostSurface = p.Surface((abs(snappedMouseX-x1),abs(snappedMouseY-y1)))
                     ghostSurface.set_alpha(128)
                     ghostSurface.fill(color_fill)
 
