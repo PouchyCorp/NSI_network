c570caa17c2e879c276c625844b02a986defeb13
GYtythub <162918388+GYtythub@users.noreply.github.com>
2024-05-10 15:17:34 +0200

    Update button.py

--- button.py
+++ button.py
@@ -1,1 +1,8 @@
 class button;
+    def __init__(self, color, x, y, width, height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = 100
+        self.height = 100
+        self.texture = playerGreyScaleSprite
